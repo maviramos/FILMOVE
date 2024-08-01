467dc3d6a93a6379ead76d79ae936eb2db2ce4ea
maviramos <vitoriamariaifc@gmail.com>
2024-08-01 10:00:00 -0300

    alterações no app.js

--- app.js
+++ app.js
@@ -33,4 +33,4 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`));
+    console.log(`Servidor rodando na porta ${PORT}`)});
